35bacffc61cc9974928b7127ba367cc9620df3ea
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-06 18:24:49 +0400

    update olc.py

--- SiestaRobot/modules/olc.py
+++ SiestaRobot/modules/olc.py
@@ -8,32 +8,23 @@ from SiestaRobot import dispatcher
 from SiestaRobot.modules.disable import DisableAbleCommandHandler
 from SiestaRobot.modules.helper_funcs.chat_status import user_admin
 
+["🥰🥰"]
 
 
 
-        await brend.edit("`Sana neden inanayım?! 🧐`")
 
-        time.sleep(0.9)
 
-        await brend.edit("`Yalan mı doğru mu öğrenelim`")
-        time.sleep(1)
-        await brend.edit("`Araştırılıyor...🔎`")
-        time.sleep(0.5)
-        await brend.edit("`Yalan🤥`")
-        time.sleep(0.1)
-        await brend.edit("`Doğru👍`")
-        time.sleep(0.1)
-        await brend.edit("`Yalan🤥`")
-        time.sleep(0.1)
-        await brend.edit("`Doğru👍`")
-        time.sleep(0.1)
-        await brend.edit("`Yalan🤥`")
-        time.sleep(0.1)
-        await brend.edit("`Doğru👍`")
-        time.sleep(0.1)
-        await brend.edit("`Yalan🤥`")
-        time.sleep(0.1)
-        await brend.edit("`Doğru👍`")
-        time.sleep(0.4)
-        await brend.edit(f'`{random.choice(["Yalan söylüyorsun❌", "Doğru söylüyorsun✅"])}\n\n%{random.randint(0, 100)}`')
-        time.sleep(0.9)
+
+OLC_HANDLER = DisableAbleCommandHandler("olc", olc, run_async=True)
+
+__mod_name__ = "Olc"
+
+__command_list__ = [
+  
+  
+    "olc",
+]
+__handlers__ = [
+
+    KILL_HANDLER,
+]
