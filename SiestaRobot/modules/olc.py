af7fc93ea49590dc64e09e430330a8255d4fa28f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-06 18:48:05 +0400

    update olc.py

--- SiestaRobot/modules/olc.py
+++ SiestaRobot/modules/olc.py
@@ -278,7 +278,7 @@ dispatcher.add_handler(MOONANIMATION_HANDLER)
 dispatcher.add_handler(CLOCKANIMATION_HANDLER)
 dispatcher.add_handler(BLOCKANIMATION_HANDLER)
 
-__mod_name__ = "Animation"
+__mod_name__ = "Olc"
 __command_list__ = [
     "love",
     "hack",
