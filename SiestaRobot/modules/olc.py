e8e3fe083ca0f148b2571fa1db25073a4e2c7c72
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-06 18:33:58 +0400

    update olc.py

--- SiestaRobot/modules/olc.py
+++ SiestaRobot/modules/olc.py
@@ -1,25 +1,55 @@
-import time
-from typing import List
+import asyncio
+import random
+from SiestaRobot.events import register
+from SiestaRobot import bot
+import os
+from SiestaRobot.cmdhelp import CmdHelp
 
-from telegram import Update
-from telegram.ext import run_async, CallbackContext
+@register(pattern="^.olc ?(.*)", outgoing=True)
+async def sevgi(event):
+    me = await bot.get_me()
+    await event.edit(f'`{random.choice(["❤️", "🧡," "💛", "💚", "💙", "💜", "🖤"])} Eşq faizi hesablanır...`')
 
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.disable import DisableAbleCommandHandler
-from SiestaRobot.modules.helper_funcs.chat_status import user_admin
+    if event.is_reply:
+        reply = await event.get_reply_message()
+        reply_user = await event.client.get_entity(reply.from_id)
+        text = event.pattern_match.group(1)
 
-["🥰🥰
+        if text == '':
+            firstUser_id = me.id
+            if me.username:
+                firstUser = f'@{me.username}'
+            else:
+                firstUser = f'[{me.first_name}](tg://user?id={me.id})'
+        else:
+            if '?id' in text:
+                firstUser_id = text.split('?id=')[1]
+            else:
+                firstUser_id = text
+            firstUser = text
 
-"]
+        if reply_user.username:
+            secondUser_id = reply_user.id
+            secondUser = f'@{reply_user.username}'
+        else:
+            secondUser_id = reply_user.id
+            secondUser = f'[{reply_user.first_name}](tg://user?id={reply_user.id})'
+    else:
+        return await event.edit('`Zəhmət olmasa bir istifadəçiyə cavab verin`')
 
+    for i in range(0,10):
+        await event.edit(f'`{random.choice(["❤️", "🧡" "💛", "💚", "💙", "💜", "🖤"])} Eşq faizi hesablanır... %{random.randint(0, 100)}`')
+        await asyncio.sleep(0.3)
+        
+    if os.path.exists(f'{firstUser_id}-{secondUser_id}.txt'):
+        yuzde = open(f'{firstUser_id}-{secondUser_id}.txt', 'r').read()
+        await event.edit(f'{firstUser} `ile` {secondUser} `arasındaki sevgi hesablandı!`\n**Nəticə:** `%{yuzde}`')
+    else:
+        yuzde = random.randint(0, 100)
+        open(f'{firstUser_id}-{secondUser_id}.txt', 'a+').write(str(yuzde))
+        await event.edit(f'{firstUser} `ile` {secondUser} `arasındaki eşq hesaplandı!`\n**Nəticə:** `%{yuzde}`')
 
-
-
-
-
-OLC_HANDLER = DisableAbleCommandHandler("olc", olc, run_async=True)
-
-__mod_name__ = "Olc"
-
-    OLC_HANDLER,
-]
+Help = CmdHelp('esqolc')
+Help.add_command('olc', None, '<birinci istifadəçi/cavab> <ikinci istifadəçi/siz>', 'Cavab verdiğiniz vəya nickini verdiğiniz kullanıcı ile sizin və ya ikinci istifadəçinin arasındaki sevgini ölçün')
+Help.add_info('`@Tagiyeff_018 tərıfindən düzəldilmişdir`')
+Help.add()
