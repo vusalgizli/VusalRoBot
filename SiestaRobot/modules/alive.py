ffaf817d959aa3421dd5a7675feb667a2d0d0209
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-09 20:17:43 +0400

    update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -20,5 +20,5 @@ async def awake(event):
   TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Siestaxbot?start=help"), Button.url("Support", "https://t.me/machinaxsupport")]]
+  BUTTON = [[Button.url("Kanal", "https://t.me/Fidowunkanali"), Button.url("Qrupumuz", "https://t.me/FidaninDunyasi")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
