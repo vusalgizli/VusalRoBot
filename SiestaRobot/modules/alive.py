88a4381e4652ddd4022a7bf9e0400d7e2bfdf865
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-09 20:20:57 +0400

    update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -14,11 +14,11 @@ PHOTO = "https://telegra.ph//file/e9be41d54fce7c2462c2c.mp4"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Salam [{event.sender.first_name}](tg://user?id={event.sender.id}), Mən Fidan Robotam.** \n\n"
-  TEXT += "💠 **I'm Working Properly** \n\n"
-  TEXT += f"💠 **My Master : [Sahib](https://t.me/HuseynH)** \n\n"
-  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
-  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
+  TEXT += "💠 **Mən düzgün işləyirəm** \n\n"
+  TEXT += f"💠 **Mənim Sahibim : [HUSEYN](https://t.me/HuseynH)** \n\n"
+  TEXT += f"💠 **Kitabxana Versiyası :** `{telever}` \n\n"
+  TEXT += f"💠 **Telefon Versiyası :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Versiyası :** `{pyrover}` \n\n"
+  TEXT += "**Məni bura əlavə etdiyiniz üçün təşəkkürlər ❤️**"
   BUTTON = [[Button.url("Kanal", "https://t.me/Fidowunkanali"), Button.url("Qrupumuz", "https://t.me/FidaninDunyasi")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
